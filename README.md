f365dff4f0fed014f76c9ecacbf90da3e59a3de3
John Broderick <johnjbroderick3@gmail.com>
2014-04-25 03:37:59 -0400

    OCR nums will not prepend by default. -n option.
    OCR numbers will no longer prepend to the filenames of downloaded songs
    by default. You can still prepend the OCR numbers using the new option,
    -n.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Downloads a range of OCRemixes off the ocremix.org site and remembers the last o
 Usage
 -----
 
-ocremix.rb [-f firstsong] [-t endsong] [-v] [-p NUMBER]
+ocremix.rb [-f firstsong] [-t endsong] [-n] [-v] [-p NUMBER]
 
 Examples:
-  ocremix.rb -v -p 2 -f 00100 -t 02000	 Will download OCR00100 to OCR02000 providing verbose info using 2 concurrent processes
+  ocremix.rb -v -n -p 2 -f 00100 -t 02000	 Will download OCR00100 to OCR02000 providing verbose info using 2 concurrent processes and prepend OCR numbers to filenames.
--- ocremix.rb
+++ ocremix.rb
@@ -15,12 +15,14 @@ BaseUrl = "http://ocremix.org/remix/OCR"
 options = OpenStruct.new
 options.verbose = false
 options.processes = 1
+options.ocr_nums = false
 
 opts = OptionParser.new do |opts|
   opts.on('-f', '--from [START]', Integer, 'Start song to download') { |from| options.from = from }
   opts.on('-t', '--to END', Integer, 'Last song to download') { |to| options.to = to }
   opts.on('-v', '--[no-]verbose', 'Output debug info') { |v| options.verbose = v }
   opts.on('-p', '--processes [COUNT]', Integer, 'Amount of processes to use') { |p| options.processes = p }
+  opts.on('-n', '--processes [COUNT]', 'Prepend OCR number to filename') { |n| options.ocr_nums = true }
 end 
 
 opts.parse!
@@ -55,7 +57,11 @@ end
     scheme, userinfo, host, port, registry, path, opaque, query, fragment = URI::split(url)
     Net::HTTP.start(host) do |http|
       result = http.get(path)
-      filename = sprintf("OCR%05d-%s",i,CGI::unescape(File.basename(path)))
+	  if options.ocr_nums
+		filename = sprintf("OCR%05d-%s",i,CGI::unescape(File.basename(path)))
+	  else
+		filename = sprintf(CGI::unescape(File.basename(path)))
+	  end
       puts "Saving to filename #{filename}" if options.verbose
       open(filename,"wb") do |file|
         file.write(result.body)
