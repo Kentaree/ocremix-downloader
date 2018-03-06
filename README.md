e827afbd0d978c98bea6522cf5424ac4e65ae636
Pieter Vos <kentaree@gmail.com>
2018-03-06 12:15:56 +0000

    Improved error handling. Improved help. Check existing files on disk and only download if checksum doesn't match Specify destination directory

--- README.md
+++ README.md
@@ -3,10 +3,28 @@ OCRemix Downloader
 
 Downloads a range of OCRemixes off the ocremix.org site and remembers the last one downloaded
 
+Requirements
+---
+Ruby 1.8+<br />
+Peach
+
+Installation
+---
+Run `bundle install` in the project's root directory to retrieve the dependencies
+
 Usage
 -----
 
-ocremix.rb [-f firstsong] [-t endsong] [-n] [-v] [-p NUMBER]
+Usage: ocremix.org [options] [destination]
+(If no destination is set, download into the current working directory)
+
+    -f, --from [START]               Start song to download
+    -t, --to END                     Last song to download
+    -v, --[no-]verbose               Output debug info
+    -p, --processes [COUNT]          Amount of concurrent processes to use
+    -n                               Prepend OCR number to filename
+    -h, --help                       Prints help
 
 Examples:
-  ocremix.rb -v -n -p 2 -f 00100 -t 02000	 Will download OCR00100 to OCR02000 providing verbose info using 2 concurrent processes and prepend OCR numbers to filenames.
+
+`ocremix.rb -vnp 2 -f 00100 -t 02000`	 Will download OCR00100 to OCR02000 providing verbose info using 2 concurrent processes and prepend OCR numbers to filenames.
--- ocremix.rb
+++ ocremix.rb
@@ -9,75 +9,102 @@ require 'cgi'
 require 'peach'
 require 'open-uri'
 
-ConfigFile = File.expand_path("~/.ocr_downloader.conf")
-BaseUrl = "http://ocremix.org/remix/OCR"
+CONFIG_FILE = File.expand_path("~/.ocr_downloader.conf")
+BASE_URL = "http://ocremix.org/remix/OCR"
 
-options = OpenStruct.new
-options.verbose = false
-options.processes = 1
-options.ocr_nums = false
+$options = OpenStruct.new(:verbose => false, :processes => 1, :ocr_nums => false)
 
-opts = OptionParser.new do |opts|
-  opts.on('-f', '--from [START]', Integer, 'Start song to download') { |from| options.from = from }
-  opts.on('-t', '--to END', Integer, 'Last song to download') { |to| options.to = to }
-  opts.on('-v', '--[no-]verbose', 'Output debug info') { |v| options.verbose = v }
-  opts.on('-p', '--processes [COUNT]', Integer, 'Amount of processes to use') { |p| options.processes = p }
-  opts.on('-n', '--processes [COUNT]', 'Prepend OCR number to filename') { |n| options.ocr_nums = true }
-end 
+def log(message)
+  puts message if $options.verbose
+end
 
-opts.parse!
-if options.from == nil && File.exists?(ConfigFile)
-  File.open(ConfigFile,"r") do |config|
-      from = config.gets()
-      options.from = from.chomp.to_i if from != nil
-      puts "Starting from number #{from}"
+OptionParser.new do |opts|
+  opts.banner = "Usage: ocremix.org [options] [destination]"
+
+  opts.on('-f', '--from [START]', Integer, 'Start song to download') {|from| $options.from = from}
+  opts.on('-t', '--to END', Integer, 'Last song to download') {|to| $options.to = to}
+  opts.on('-v', '--[no-]verbose', 'Output debug info') {|v| $options.verbose = v}
+  opts.on('-p', '--processes [COUNT]', Integer, 'Amount of processes to use') {|p| $options.processes = p}
+  opts.on('-n', 'Prepend OCR number to filename') {|n| $options.ocr_nums = true}
+  opts.on('-h', '--help', 'Prints this help') do
+    puts opts
+    exit
+  end
+end.parse!
+
+destination = ARGV.empty? ? Dir.pwd : ARGV.join
+Dir.chdir destination
+
+if $options.from == nil && File.exists?(CONFIG_FILE)
+  File.open(CONFIG_FILE, "r") do |config|
+    from = config.gets
+    $options.from = from.chomp.to_i if from != nil
+    puts "Starting from number #{from}"
   end
 end
 
-(options.from..options.to).to_a.peach(options.processes) do |i|
-  url = sprintf("%s%05d/",BaseUrl,i)
-  puts url if options.verbose
-  result = open(url){|f|f.read}
+($options.from..$options.to).to_a.peach($options.processes) do |i|
+  url = sprintf("%s%05d/", BASE_URL, i)
+  log(url)
+
+  begin
+    result = open(url) do |f|
+      f.read
+    end
+  rescue OpenURI::HTTPError => error
+    puts "Couldn't find OCR #{i}, status code #{error.io.status}"
+    next
+  end
+
   matches = result.scan(/MD5 Checksum: <\/strong>([^<]+)</)
   if matches == nil || matches[0] == nil
-    puts sprintf("Skipping OCR%05d",i) if options.verbose
+    log sprintf("Skipping OCR%05d", i)
     next
   end
 
   md5 = matches[0][0].chomp
-  puts "Md5: #{md5}" if options.verbose
-    
+  log "Md5: #{md5}"
+
   matches = result.scan(/<a href="([^"]+)">Download from/)
   success = false
   matches.each do |match|
     url = match[0]
-    puts "Url: #{url}, type: #{url.class}" if options.verbose
-    uri = URI.parse(url)
-
-    scheme, userinfo, host, port, registry, path, opaque, query, fragment = URI::split(url)
+    log "Url: #{url}, type: #{url.class}"
+    _, _, host, _, _, path, _, _, _ = URI::split(url)
     Net::HTTP.start(host) do |http|
       result = http.get(path)
-	  if options.ocr_nums
-		filename = sprintf("OCR%05d-%s",i,CGI::unescape(File.basename(path)))
-	  else
-		filename = sprintf(CGI::unescape(File.basename(path)))
-	  end
-      puts "Saving to filename #{filename}" if options.verbose
-      open(filename,"wb") do |file|
+      if $options.ocr_nums
+        filename = sprintf("OCR%05d-%s", i, CGI::unescape(File.basename(path)))
+      else
+        filename = sprintf(CGI::unescape(File.basename(path)))
+      end
+
+      if File.exists?(filename)
+        log "File exists, comparing checksum"
+        digest = Digest::MD5.hexdigest(File.read(filename))
+        if md5 == digest
+          puts "File already exists with same checksum, skipping"
+          success = true
+          break
+        end
+      end
+
+      log "Saving to filename #{filename}"
+      open(filename, "wb") do |file|
         file.write(result.body)
       end
       digest = Digest::MD5.hexdigest(File.read(filename))
-      puts "Expected md5: #{md5}, actual: #{digest}" if options.verbose
+      log "Expected md5: #{md5}, actual: #{digest}"
       if md5 == digest
-        puts "Download successful!" if options.verbose
+        log "Download successful!"
         success = true
       end
     end
     break if success
   end
 
-  File.open(ConfigFile,"w") do |config|
-    config.puts(i+1)
+  File.open(CONFIG_FILE, "w") do |config|
+    config.puts(i + 1)
   end
 end
-   
+
